616c3f4631e1896e358379d1af59566b804a5ec4
Arek Sanecki <areksanecki@gmail.com>
2023-03-20 19:07:07 +0100

    add class Element

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+class Element:
+    def __init__(self):
+        self.val = 0
+
+    def setVal(self, val):
+        self.val = val
+
+    def delVal(self):
+        self.val = 0
+
+    def getVal(self):
+        return self.val
+
+
 def main():
     pass
+
 
 if __name__ == '__main__':
     main()
