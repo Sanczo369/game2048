29021b170e60a23d25e732a5acf837b5a9511e16
Arek Sanecki <areksanecki@gmail.com>
2023-03-20 19:18:53 +0100

    add class Board and Game

--- main.py
+++ main.py
@@ -11,6 +11,31 @@ class Element:
     def getVal(self):
         return self.val
 
+class Board:
+    def __init__(self):
+        pass
+    def randomizeElement(self):
+        pass
+    def right(self):
+        pass
+    def left(self):
+        pass
+    def up(self):
+        pass
+    def down(self):
+        pass
+    def getTable(self):
+        pass
+    def copyTable(self):
+        pass
+class Game:
+    def __init__(self):
+        pass
+    def draw(self, table):
+        pass
+    def run(self, board):
+        pass
+
 
 def main():
     pass
