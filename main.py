a9d4b6b7c16cf44ba536ca06b13f02f4ab478a1e
Arek Sanecki <areksanecki@gmail.com>
2023-03-21 19:41:06 +0100

    draw() in class Game

--- main.py
+++ main.py
@@ -114,12 +114,19 @@ class Game:
     def __init__(self):
         pygame.init()
         self.screen = pygame.display.set_mode([400, 400])
-        pygame.display.set_caption("Gra 2048")
+        pygame.display.set_caption("Game 2048")
         pygame.font.init()
         self.myfont30 = pygame.font.SysFont("ComicSans", 30)
         self.myfont45 = pygame.font.SysFont("ComicSans", 45)
     def draw(self, table):
-        pass
+        self.screen.fill((0, 0, 0))
+        text = self.myfont45.render("Nasza Gra", False, (128, 255, 0))
+        self.screen.blit(text, (0, 0))
+        for x in range(0, 4):
+            for y in range(0, 4):
+                text = self.myfont30.render(str(table[y][x].getVal()), False, (255, 255, 0))
+                self.screen.blit(text, (x*100+20, y*75+100))
+        pygame.display.flip()
     def run(self, board):
         pass
 
