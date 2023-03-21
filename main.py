0fea90d1df4a732a0fb8e3b10deaffeb5976d4d4
Arek Sanecki <areksanecki@gmail.com>
2023-03-21 15:58:01 +0100

    add move functions in class Board

--- main.py
+++ main.py
@@ -32,14 +32,77 @@ class Board:
             x = random.randint(0, 3)
             y = random.randint(0, 3)
         self.board[x][y].setVal(random.randint(1, 2)*2)
+
+    #move
     def right(self):
-        pass
+        self.copyTable()
+        for z in range(0, 3):
+            for x in range(0, 3):
+                for y in range(0, 4):
+                    x = 2-x
+                    if self.board[y][x+1].getVal() == 0:
+                        self.board[y][x+1].setVal(self.board[y][x].getVal())
+                        self.board[y][x].delVal()
+                    elif self.board[y][x].getVal() == self.board[y][x+1].getVal():
+                        self.board[y][x + 1].setVal(self.board[y][x].getVal()*2)
+                        self.board[y][x].delVal()
+        for x in range(0, 4):
+            for y in range(0, 4):
+                if self.prevBoard[y][x].getVal() != self.board[y][x].getVal():
+                    return True
+        return False
+
     def left(self):
-        pass
+        self.copyTable()
+        for z in range(0, 3):
+            for x in range(1, 4):
+                for y in range(0, 4):
+                    if self.board[y][x - 1].getVal() == 0:
+                        self.board[y][x - 1].setVal(self.board[y][x].getVal())
+                        self.board[y][x].delVal()
+                    elif self.board[y][x].getVal() == self.board[y][x - 1].getVal():
+                        self.board[y][x - 1].setVal(self.board[y][x].getVal() * 2)
+                        self.board[y][x].delVal()
+        for x in range(0, 4):
+            for y in range(0, 4):
+                if self.prevBoard[y][x].getVal() != self.board[y][x].getVal():
+                    return True
+        return False
+
     def up(self):
-        pass
+        self.copyTable()
+        for z in range(0, 3):
+            for x in range(0, 4):
+                for y in range(1, 4):
+                    if self.board[y-1][x].getVal() == 0:
+                        self.board[y-1][x].setVal(self.board[y][x].getVal())
+                        self.board[y][x].delVal()
+                    elif self.board[y][x].getVal() == self.board[y-1][x].getVal():
+                        self.board[y-1][x].setVal(self.board[y][x].getVal() * 2)
+                        self.board[y][x].delVal()
+        for x in range(0, 4):
+            for y in range(0, 4):
+                if self.prevBoard[y][x].getVal() != self.board[y][x].getVal():
+                    return True
+        return False
+
     def down(self):
-        pass
+        self.copyTable()
+        for z in range(0, 3):
+            for x in range(0, 4):
+                for y in range(0, 3):
+                    y = 2-y
+                    if self.board[y+1][x].getVal() == 0:
+                        self.board[y+1][x].setVal(self.board[y][x].getVal())
+                        self.board[y][x].delVal()
+                    elif self.board[y][x].getVal() == self.board[y+1][x].getVal():
+                        self.board[y+1][x].setVal(self.board[y][x].getVal()*2)
+                        self.board[y][x].delVal()
+        for x in range(0, 4):
+            for y in range(0, 4):
+                if self.prevBoard[y][x].getVal() != self.board[y][x].getVal():
+                    return True
+        return False
     def getTable(self):
         pass
     def copyTable(self):
